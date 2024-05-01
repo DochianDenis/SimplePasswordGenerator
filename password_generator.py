b8aaf68cffcfe4df57d6ddc5dca588f2d551deb9
DochianDenis <denisdochian.andrei1996@gmail.com>
2024-05-01 21:47:56 +0300

    Simplified the script by removing some unnecessary lines

--- password_generator.py
+++ password_generator.py
@@ -2,20 +2,11 @@ import string
 import random
 import pyperclip
 
-# Define character sets
-lowercase_letters = string.ascii_lowercase
-uppercase_letters = string.ascii_uppercase
-digits = string.digits
-special_characters = "!@#$%^&*()_+-=[]{}|;:,.<>?/~"
+characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + "!@#$%^&*()_+-=[]{}|;:,.<>?/~"
 
-# Combine all character sets
-all_characters = lowercase_letters + uppercase_letters + digits + special_characters
+passwordLength = int(input("The length of the password:"))
+password = ''.join(random.choices(characters, k = passwordLength))
 
-# Generate a password of length (input from the user) using characters from the combined set
-password_length = int(input("The length of the password:"))
-password = ''.join(random.choices(all_characters, k = password_length))
-
-# Saves the generated password to the clipboard.
 pyperclip.copy(password)
-
 print(password)
+
